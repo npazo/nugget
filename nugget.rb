5bf24465f4dc96478470e5528e47afe0bc27a5e2
Nick Pazoles <np7776@gmail.com>
2014-08-04 19:43:49 -0500

    add support for monthly galleries

--- nugget.rb
+++ nugget.rb
@@ -5,11 +5,19 @@ $thumbs_dir = "/path/to/thumbnails"
 # path to the full size photos with a trailing slash
 $full_dir = "/path/to/photo_export/"
 
+# change whether galleries should be monthly instead of yearly (recommended for very large galleries)
+$monthly = false
+
+##### END OF SETTINGS #####
+
+$last_output = "index.html"
+
 def read_dir (dir, output, index, full_dir_path)
   count = 0		# keeps track of pictures in a row so it knows when to create a new one
   pic_count = 0
   first_month = true
   first_year = true
+  start_new = false
   Dir.glob("#{dir}/*").each_with_object({}) do |f, h|
     if File.file?(f)
       if count == 0
@@ -24,32 +32,29 @@ def read_dir (dir, output, index, full_dir_path)
         count = 0
       end
     elsif File.directory?(f)
-
-      if File.basename(f).include? "-"	# this is how it checks for a month folder
+      if File.basename(f).include? "-"	# this is how it checks for a month folder        
         if count > 0 
           count = 0
         end
       
-        if !first_month
+        if !first_month && !$monthly
            output << "</div></div>"	# end the month as long as there is one
         end
-        output << "<h2>" + File.basename(f) + "</h2>"
+        
+        ## don't output the next month if the galleries are set to monthly
+        if !$monthly
+          output << "<h2>" + File.basename(f) + "</h2>"
+        end
        
         first_month = false
+        start_new = false
       else # if its a top level year
-        
+        start_new = true
+      end
+      
+      if (start_new ^ $monthly)
         index << "<h1><a href=\"" + File.basename(f) + ".html\">" + File.basename(f) + "</a></h1>"
-         
-        if !first_year
-          output << "</div></div>"
-          output << "</div>"
-          output << "<div id=\"nav\">"
-          output << "<div id=\"prev\"><a href=\"" + (File.basename(f).to_i - 1).to_s + ".html" + "\"><< Prev</a></div>"
-          output << "<div id=\"next\"><a href=\"" + File.basename(f) + ".html" + "\">Next >></a></div>"
-          output << "</div>"
-          output.close
-        end
-  
+          
         output = File.open( File.basename(f) + ".html", "w" )
         
         output << "<html>"
@@ -60,15 +65,39 @@ def read_dir (dir, output, index, full_dir_path)
         output << "</head>"
         output << "<body>"
         output << "<div id=\"main\">"
-        
-  
+           
         output << "<h1>" + File.basename(f) + "</h1>"
+
         first_year = false
       end
-        read_dir(f, output, index, full_dir_path + File.basename(f) + "/")
+      
+      read_dir(f, output, index, full_dir_path + File.basename(f) + "/")
+
+      if !first_year && (start_new || $monthly)
+        output << "</div></div>"
+        output << "</div>"
+        output << "<div id=\"nav\">"
+        output << "<div id=\"prev\"><a href=\"" + $last_output + "\"><< Prev</a></div>"
+        output << "<div id=\"next\"><a href=\"{NEXT_FILE}.html" + "\">Next >></a></div>"
+        output << "</div>"
+        output << "</body></html>"
+        output.close
+        
+        update_nav($last_output, File.basename(f))
+        $last_output = File.basename(f) + ".html"
+      end
     end
   end
 end
+
+def update_nav(file_to_process, next_file)
+  outdata = File.read(file_to_process).gsub("{NEXT_FILE}", next_file)
+  
+  File.open(file_to_process, 'w') do |out|
+    out << outdata
+  end  
+end
+
 
 index = File.open( "index.html", "w" )
 output = index
@@ -82,9 +111,5 @@ index << "</head>"
 index << "<body>"
 
 read_dir($thumbs_dir, output, index, $full_dir)
-
-# close the last month
-output << "</div></div></body>"
-output << "</html>"
 
 output.close
